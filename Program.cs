8e50a73ec2dd3b87670eb6318afaeb43c7b7bad6
Felipe Mata <felipeg.zym@gmail.com>
2023-04-24 18:59:52 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.OpenApi.Models;
+using PizzaStore.Db;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo 
+    { 
+        Title = "PizzaStore API", 
+        Description = "A sua pizza está aqui!"
+    });
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API v1");
+});
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/pizzas/{id}", (int id) => PizzaDb.GetPizza(id));
+app.MapGet("/pizzas", () => PizzaDb.GetPizzas());
+app.MapPost("/pizzas", (Pizza pizza) => PizzaDb.CreatePizza(pizza));
+app.MapPut("/pizzas", (Pizza pizza) => PizzaDb.UpdatePizza(pizza));
+app.MapDelete("/pizzas/{id}", (int id) => PizzaDb.RemovePizza(id));
+
+app.Run();
